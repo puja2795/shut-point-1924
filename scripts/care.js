a5208d84ab8ac80730b8a2ed42496314f05eeb2f
yashsrivastava25 <113158835+yashsrivastava25@users.noreply.github.com>
2022-11-10 20:04:11 +0530

    day-3

--- scripts/care.js
+++ scripts/care.js
@@ -23,3 +23,92 @@ i++;
 
 }
 crousel();
+let obj=[{id:1,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/diagnostics/screening-packs/sr_citizen_adv_m_mag.png",
+    price:890
+
+},
+{id:2,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/diagnostics/screening-packs/sr_citizen_adv_f_mag.png",
+    price:790
+
+},
+{
+    id:3,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/IronScreeningClp122020.png",
+    price:390
+},{id:4,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/WomensHealthScreening_Magazine.png",
+    price:1790
+
+},{id:5,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/diagnostics/screening-packs/PCODMagazineWeb.png",
+    price:1790
+
+},{
+    id:6,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/mp/diag_pack_Diabetes_Screening_clp.jpg",
+    price:2990
+},{
+    id:7,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/LipidScreening_Magazine.png",
+    price:2990
+},{
+    id:8,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/ImmunityCcreening_Magazine.png",
+    price:590
+},{id:9,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/CF01032_hero_2.png",
+    price:1490
+
+},{
+    id:10,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/PregnancyScreening_Magazine.png",
+    price:1990
+},{
+    id:11,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/mp/diag_pack_Kidney_Screening_clp.jpg",
+    price:890
+},{id:12,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/HairFallScreeningClp122019.png",
+    price:890
+
+},{
+    id:13,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/mp/diag_pack_Liver_Screening_clp.jpg",
+    price:890
+},{
+    id:14,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/FeverPanel_Magazine.jpg",
+    price:890
+},{
+    id:15,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/Alcohol_magazine.png",
+    price:890
+},{
+    id:16,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/diagnostics/screening-packs/Anemia_Magazine_Web_new.png",
+    price:890
+},{
+    id:17,
+    image_url:"https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,fl_progressive,f_auto,q_auto:eco/dpr_2/image/carefit/bundle/Obesity_Risk_Magazine.jpg",
+    price:890
+},
+
+]
+obj.forEach(function(el){
+    let card=document.createElement("div");
+    let productImg=document.createElement("img");
+    productImg.src=el.image_url;
+    let price=document.createElement("p");
+    price.innerText=el.price;
+    let btn =document.createElement("button");
+   btn.innerText="Add to cart";
+    btn.addEventListener("click", addtocart);
+    card.append(productImg,price,btn);
+    document.querySelector("#container").append(card);
+
+    function addtocart(){
+        console.log("inside card button")
+    }
+})
