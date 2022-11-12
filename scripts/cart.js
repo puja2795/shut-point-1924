e0b1f8e4f3ef21447cdc2b48bbe2b320fd5f2986
puja2795 <pujarinisahoo95@gmail.com>
2022-11-12 21:02:38 +0530

    all files arranged

---
+++ scripts/cart.js
@@ -0,0 +1,23 @@
+let selectedtest=JSON.parse(localStorage.getItem("selected_item"))
+let carts=document.getElementById("carts");
+   
+let largeImg=document.createElement("img");
+largeImg.src=selectedtest.largeimg_url;
+
+let title = document.createElement("h2");
+title.innerText = selectedtest.title;
+
+let des=document.createElement("p");
+des.innerText=selectedtest.description;
+
+let price=document.createElement("p")
+price.innerText=selectedtest.price;
+
+let addToCart = document.createElement("button");
+addToCart.innerText = "ADD";
+addToCart.addEventListener("click", () => {
+    
+})
+
+carts.append(largeImg,title,des,price,addToCart);
+    
