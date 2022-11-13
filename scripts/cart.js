1c70690565e0407b797b3f9efcfd05419b1c408a
yashsrivastava25 <113158835+yashsrivastava25@users.noreply.github.com>
2022-11-13 13:35:47 +0530

    day-5

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,8 +2,7 @@ import {navbar, locateDiv, navbarFunc, cartDiv, showCartItems} from "../componen
 import {footerDiv, optionalFooter} from "../component/footer.js";
 
 
-let selectedtest=JSON.parse(localStorage.getItem("selected_item"))
-
+let selectedtest=JSON.parse(localStorage.getItem("selected_item"));
 
 
 let carts=document.getElementById("carts");
